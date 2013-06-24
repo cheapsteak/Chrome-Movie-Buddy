9a7ac3ea5a92a8d124c4fe54c9de219ebd6a06fa
Chang Wang <garnwraly@gmail.com>
2013-06-23 21:17:18 -0400

    Added a few more places to inject ratings. Movies for which RT ratings can't be found will display 'check freshness' link

--- moviebuddy.js
+++ moviebuddy.js
@@ -1,6 +1,7 @@
 var blacklist = [
     ': Director\'s Cut - A Most Wanted Mondays Presentation',
     ': An IMAX 3D Experience®',
+    ': The IMAX Experience®',
     '- A Sinister Cinema Presentation'
 ];
 
@@ -16,15 +17,74 @@ var findTomato = function (title, tomatoes) {
     }
 };
 
+var namespaceClasses = function (classes) {
+    return classes.map(function (mbclass) {return "mb--" + mbclass;}).join(' ');
+};
+
+var prepareData = function (tomato, classes) {
+    return {
+        audience_score: tomato.ratings.audience_score,
+        critics_score: tomato.ratings.critics_score,
+        critics_rating: tomato.ratings.critics_rating,
+        audience_rating: tomato.ratings.audience_rating,
+        link: tomato.links.alternate,
+        title: tomato.title,
+        classes: namespaceClasses(classes)
+    };
+};
+
 var injectRatings = function(tomatoes){
-    $('.movie-link h3').each(function(i, el){
-        var title = $(el).text().replace(blacklistRegexp, '').trim();
-        var tomato = findTomato(title, tomatoes);
-        if (tomato) {
-            $(el).closest('.moviedetailsmaininfo').prepend(Handlebars.templates.ratings(tomato.ratings));
-        }
-    });
-}
+    switch (namespace) {
+        case "cineplex":
+
+            //first on top : http://cineplex.com/Theatres/TheatreDetails/Cineplex-Cinemas-Mississauga-formerly-Coliseum-Mississauga-.aspx
+            $('.FFEC-Display').css('position','relative');
+            $('.FFEC-Display').each(function(i, el){
+                var title = $(el).find('h3').text().replace(blacklistRegexp, '').trim();
+                var tomato = findTomato(title, tomatoes);
+                var classes = ["poster-width", "overlay"];
+                var templateData = tomato ? prepareData(tomato, classes) : { query: title, classes: namespaceClasses(classes) };
+                $(el).find('.movie-link').prepend(Handlebars.templates.ratings(templateData));
+            });
+            //http://cineplex.com/Theatres/TheatreDetails/Cineplex-Cinemas-Mississauga-formerly-Coliseum-Mississauga-.aspx
+            $('.Listing .moviedetailsmaininfo').css('position', 'relative');
+            $('.Listing .moviedetailsmaininfo').each(function(i, el){
+                var title = $(el).find('h3').text().replace(blacklistRegexp, '').trim();
+                var tomato = findTomato(title, tomatoes);
+                var classes = ["inline", "poster-width", "text-center", "below-poster"];
+                var templateData = tomato ? prepareData(tomato, classes) : { query: title, classes: namespaceClasses(classes) };
+                $(el).find('.Poster').after(Handlebars.templates.ratings(templateData));
+            });
+            //homepage
+            $('.BoxOfficeMovie').each(function(i, el){
+                var title = $(el).find('a').text().replace(blacklistRegexp, '').trim();
+                var tomato = findTomato(title, tomatoes);
+                var classes = ["inline", "float-right"];
+                var templateData = tomato ? prepareData(tomato, classes) : { query: title, classes: namespaceClasses(classes) };
+                $(el).append(Handlebars.templates.ratings(templateData));
+            });
+            //homepage
+            $('.MovieItemsRotator .item').each(function(i, el){
+                var title = $(el).find('.Title').text().replace(blacklistRegexp, '').trim();
+                var tomato = findTomato(title, tomatoes);
+                var classes = ["overlay"];
+                var templateData = tomato ? prepareData(tomato, classes) : { query: title, classes: namespaceClasses(classes) };
+                $(el).find('div').first().css('position','relative').prepend(Handlebars.templates.ratings(templateData));
+            });
+            //http://cineplex.com/Movies.aspx
+            $('.Movies .Movie').each(function(i, el){
+                var title = $(el).find('h3').text().replace(blacklistRegexp, '').trim();
+                var tomato = findTomato(title, tomatoes);
+                var classes = ["inline", "full-center"];
+                var templateData = tomato ? prepareData(tomato, classes) : { query: title, classes: namespaceClasses(classes) };
+                $(el).find('.MoviePoster').append(Handlebars.templates.ratings(templateData));
+            });
+            break;
+        case "netflix":
+            break;
+    }
+    
+};
 
 $(function(){
     var requestInTheatres = $.getJSON("//api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?page_limit=20&page=1&country=ca&apikey="+apiKey);
@@ -32,7 +92,7 @@ $(function(){
     $.when(requestInTheatres, requestOpeningMovies)
     .done(function (data1, data2) {
         var tomatoes = _.union(data1[0].movies, data2[0].movies);
-        console.log(tomatoes);
         injectRatings(tomatoes);
+        console.log(tomatoes);
     });
 });
--- ratings.template.js
+++ ratings.template.js
@@ -3,10 +3,20 @@
 templates['ratings'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [3,'>= 1.0.0-rc.4'];
 helpers = helpers || Handlebars.helpers; data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
-
-  buffer += "<div class=\"mb-scores\">\r\n	<div class=\"mb-critics\"><span class=\"mb-icon ";
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\r\n	<div class=\"mb-critics\"><a href=\"";
+  if (stack1 = helpers.link) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.link; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" title=\"See reviews for '";
+  if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "' at Rotten Tomatoes\"><span class=\"mb-icon ";
   if (stack1 = helpers.critics_rating) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.critics_rating; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
@@ -18,7 +28,15 @@ helpers = helpers || Handlebars.helpers; data = data || {};
   if (stack1 = helpers.critics_score) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.critics_score; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "%</span></div>\r\n	<div class=\"mb-audience\"><span class=\"mb-icon ";
+    + "%</span></a></div>\r\n	<div class=\"mb-audience\"><a href=\"";
+  if (stack1 = helpers.link) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.link; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" title=\"See reviews for '";
+  if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "' at Rotten Tomatoes\"><span class=\"mb-icon ";
   if (stack1 = helpers.audience_rating) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.audience_rating; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
@@ -30,7 +48,29 @@ helpers = helpers || Handlebars.helpers; data = data || {};
   if (stack1 = helpers.audience_score) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.audience_score; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "%</span></div>\r\n</div>";
+    + "%</span></a></div>\r\n	";
+  return buffer;
+  }
+
+function program3(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\r\n	<div class=\"mb-not-found\"><a href=\"https://www.rottentomatoes.com/search/?search=";
+  if (stack1 = helpers.query) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.query; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" target=\"rt\">check freshness</a></div>\r\n	";
+  return buffer;
+  }
+
+  buffer += "<div class=\"mb-scores ";
+  if (stack1 = helpers.classes) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.classes; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\">\r\n	";
+  stack1 = helpers['if'].call(depth0, depth0.critics_score, {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\r\n</div>";
   return buffer;
   });
 })();
